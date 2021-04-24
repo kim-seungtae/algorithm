5d895514f4ce6fd7a68154235dece02afc498ba4
tae1303 <tae1303@gmail.com>
2021-04-24 22:57:53 +0900

    Solve boj 20058

--- Simulation/boj_20058.cpp
+++ Simulation/boj_20058.cpp
@@ -1,103 +1,80 @@
 #include <iostream>
-#include <stack>
-#define MAX_N       6
-#define MAX_L       64
-#define MAX_K       1000
+#define MAX_N       64
+#define MAX_Q       1000
 using namespace std;
-
-typedef pair<int, int> pii;
-int dy[4] = {-1,0,1,0};
-int dx[4] = {0,1,0,-1};
-int max(int a, int b) {return a>b?a:b;}
-int N, Q, L[MAX_K];
-int ice[MAX_L][MAX_L], tmp[MAX_L][MAX_L];
-bool visited[MAX_L][MAX_L] = {false, };
-bool isIce[MAX_L][MAX_L];
-
-void input() {
-    cin >> N >> Q;
-    for (int i=0; i<(1<<N); ++i) for (int j=0; j<(1<<N); ++j) {
-        cin >> ice[i][j];
-    }
-    for (int i=0; i<Q; ++i) cin >> L[i];
-}
-void rotate(int pos, int len) {
-    int y = pos / (1<<N);
-    int x = pos % (1<<N);
-    for (int i=y; i<y+len; ++i) for (int j=x; j<x+len; ++j) {
-        tmp[i][j] = ice[i][j];
-    }
-    for (int i=0; i<len; ++i) {
-        for (int j=0; j<len; ++j) {
-            ice[j+y][len-i-1+x] = tmp[i+y][j+x];
+int dy[4] = {-1,0,1,0}, dx[4] = {0,1,0,-1};
+int N, Q;
+int L[MAX_Q], ice[2][MAX_N][MAX_N], rTmp[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+void rotate(int I, int J, int L, int p) {
+    for (int i=0; i<L; ++i) for (int j=0; j<L; ++j) rTmp[i][j] = ice[p][I+i][J+j];
+    for (int i=0; i<L; ++i) {
+        for (int j=0; j<L; ++j) {
+            ice[p][I+j][J+L-i-1] = rTmp[i][j];
         }
     }
 }
-void minusIce() {
-    for (int i=0; i<(1<<N); ++i) for (int j=0; j<(1<<N); ++j) {
-        isIce[i][j] = false;
-        if (ice[i][j]) isIce[i][j] = true;
-    }
-
-    for (int i=0; i<(1<<N); ++i) for (int j=0; j<(1<<N); ++j) {
-        if (!isIce[i][j]) continue;
-        int num = 0;
-        for (int k=0; k<4; ++k) {
-            int ny = i + dy[k];
-            int nx = j + dx[k];
-
-            if (ny<0||ny>=(1<<N)||nx<0||nx>=(1<<N)||!isIce[ny][nx]) continue;
-            ++num;
-        }
-        if (num < 3) --ice[i][j];
+void tornado(int L, int p) {
+    for (int i=0; i<N; i+=L) for (int j=0; j<N; j+=L) {
+        rotate(i, j, L, p);
     }
 }
-int findSizeOfLargestIce() {
-    int M = 0;
-    for (int i=0; i<(1<<N); ++i) for (int j=0; j<(1<<N); ++j) {
-        if (ice[i][j] == 0 || visited[i][j]) continue;
-        stack<pii> s;
-        s.push({i, j});
+void fireball(int p) {
+    int np = (p + 1) % 2;
+    for (int i=0; i<N; ++i) for (int j=0; j<N; ++j) {
+        if (ice[p][i][j] == 0) {
+            ice[np][i][j] = 0;
+            continue;
+        }
+        int adj = 0;
+        for (int d=0; d<4; ++d) {
+            int ny = i + dy[d], nx = j + dx[d];
+            if (ny<0||ny>=N||nx<0||nx>=N||ice[p][ny][nx] == 0) continue;
+            ++adj;
+        }
+        if (adj>=3) ice[np][i][j] = ice[p][i][j];
+        else ice[np][i][j] = ice[p][i][j] - 1;
+    }
+}
+int getRemainIce(int p) {
+    int ret = 0;
+    for (int i=0; i<N; ++i) for (int j=0; j<N; ++j) {
+        ret += ice[p][i][j];
+    }
+    return ret;
+}
+int dfs(int y, int x, int p) {
+    int ret = 1;
+    for (int d=0; d<4; ++d) {
+        int ny = y + dy[d], nx = x + dx[d];
+
+        if (ny<0||ny>=N||nx<0||nx>=N||visited[ny][nx]||ice[p][ny][nx] == 0) continue;
+        visited[ny][nx] = true;
+        ret += dfs(ny, nx, p);
+    }
+    return ret;
+}
+int getBiggestIce(int p) {
+    int ret = 0;
+    for (int i=0; i<N; ++i) for (int j=0; j<N; ++j) {
+        if (ice[p][i][j] == 0 || visited[i][j]) continue;
         visited[i][j] = true;
-        int tmp = 0;
-        while (!s.empty()) {
-            int y = s.top().first;
-            int x = s.top().second;
-            s.pop();
-            ++tmp;
-
-            for (int i=0; i<4; ++i) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if (ny<0||nx<0||ny>=(1<<N)||nx>=(1<<N)||ice[ny][nx]==0||visited[ny][nx]) continue;
-                s.push({ny, nx});
-                visited[ny][nx] = true;
-            }
-        }
-        M = max(tmp, M);
+        ret = max(ret, dfs(i, j, p));
     }
-    return M;
-}
-int getTotalNumOfIce() {
-    int sum = 0;
-    for (int i=0; i<(1<<N); ++i) for (int j=0; j<(1<<N); ++j) {
-        sum += ice[i][j];
-    }
-    return sum;
+    return ret;
 }
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
+    cin >> N >> Q;
+    N = 1 << N;
+    for (int i=0; i<N; ++i) for (int j=0; j<N; ++j) cin >> ice[0][i][j];
+    for (int i=0; i<Q; ++i) cin >> L[i];
 
-    input();
+    int p = 0;
     for (int i=0; i<Q; ++i) {
-        for (int j=0; j<(1<<N); j+=(1<<L[i])) {
-            for (int k=0; k<(1<<N); k+=(1<<L[i])) {
-                rotate(j*(1<<N)+k, (1<<L[i]));
-            }
-        }
-        minusIce();
+        tornado(1<<L[i], p);
+        fireball(p);
+        p = (p + 1) % 2;
     }
-    cout << getTotalNumOfIce() << "\n" << findSizeOfLargestIce() << endl;
+    cout << getRemainIce(p) << '\n' << getBiggestIce(p) << '\n';
     return 0;
 }
